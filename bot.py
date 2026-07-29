cb3dca3f9b0538c5325d5b936b6b4a4f47bb355e
ilsina-93-hue <ilsina-93@mail.ru>
2026-07-29 17:05:15 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,8 @@ import telebot
 import json
 import random
 import os
+import threading
+from flask import Flask
 
 TOKEN = "8969226485:AAF5agI6z1HHj1pHN4Usj-Q30joFRiHbcQM"
 
@@ -12,15 +14,36 @@ with open("characters.json", "r", encoding="utf-8-sig") as file:
     characters = json.load(file)
 
 
+# -----------------------
+# Render Web Server
+# -----------------------
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "HoneyRealmBot работает!"
+
+
+def run_web():
+    app.run(host="0.0.0.0", port=10000)
+
+
+# -----------------------
+# Telegram Bot
+# -----------------------
+
 @bot.message_handler(commands=["start"])
 def start(message):
     markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+
     button = telebot.types.KeyboardButton("🍯 Узнать, кто я")
     markup.add(button)
 
     bot.send_message(
         message.chat.id,
-        "*Добро пожаловать в HoneyRealm* 🍯\nНажми кнопку и узнай своего персонажа!",
+        "*Добро пожаловать в HoneyRealm* 🍯\n\nНажми кнопку и узнай своего персонажа!",
         reply_markup=markup,
         parse_mode="Markdown"
     )
@@ -28,12 +51,15 @@ def start(message):
 
 @bot.message_handler(func=lambda message: message.text == "🍯 Узнать, кто я")
 def get_character(message):
+
     character = random.choice(characters)
 
     image_name = character["image"]
 
-    # Путь к картинке
-    image_path = os.path.join(os.path.dirname(__file__), image_name)
+    image_path = os.path.join(
+        os.path.dirname(__file__),
+        image_name
+    )
 
     caption = (
         f"🍯 *{character['name']}*\n\n"
@@ -41,6 +67,7 @@ def get_character(message):
     )
 
     if os.path.exists(image_path):
+
         with open(image_path, "rb") as photo:
             bot.send_photo(
                 message.chat.id,
@@ -48,7 +75,9 @@ def get_character(message):
                 caption=caption,
                 parse_mode="Markdown"
             )
+
     else:
+
         bot.send_message(
             message.chat.id,
             f"Ошибка: не найдена картинка {image_name}"
@@ -57,4 +86,12 @@ def get_character(message):
 
 print("HoneyRealmBot запущен!")
 
+
+# Запускаем порт для Render
+threading.Thread(
+    target=run_web
+).start()
+
+
+# Запускаем Telegram
 bot.infinity_polling()
